a8f9d7dcf12457db418dde9890119b375c5bbfe2
Sam Eads <sam.eads@gmail.com>
2024-08-10 11:33:59 -0400

    start page complete

---
+++ src/components/chat-bot-start.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './chat-bot-start.css';
+
+const ChatBotStart = () => {
+    return (
+
+        <div className='start-page'>
+            <button className='start-page-btn'>CHAT AI</button>
+        </div>
+
+    )
+}
+
+export default ChatBotStart;
